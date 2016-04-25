5bede8a260775fa4b236c43293aa6e5e27aaf6f1
vagrant <vagrant@homestead>
2016-04-25 04:04:39 +0000

    Add a new line to end of file

--- resources/views/backend/brands/edit.blade.php
+++ resources/views/backend/brands/edit.blade.php
@@ -3,50 +3,50 @@
 @section('title', 'Update Brand')
 
 @section('content')
-<div id="page-wrapper">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Update brand</h1>
-                </div>
-                <!-- /.col-lg-12 -->
+    <div id="page-wrapper">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Update brand</h1>
             </div>
-            <!-- /.row -->
-            <div class="row"> 
-                <div class="col-lg-12">
-                    <div class="panel panel-default">
-                        <div class="panel-body">
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <form role="form" action="{{ url('admin/brands/'.$brand['id']) }}" method="post">
-                                        {{ csrf_field() }}
-                                        {{ method_field('patch') }}
-                                        <div class="form-group">
-                                            <label>Text Input</label>
-                                            <input class="form-control" name="brand_name" value="{{ $brand['brand_name'] }}">
-                                        </div>
-                                        <button type="submit" class="btn btn-primary">Update</button>
-                                    </form>
-                                </div>
-                                <!-- /.col-lg-12 (nested) -->
-                            </div>
-                            <!-- /.row (nested) -->
-                        </div>
-                        @if(count($errors) > 0)
-                            <div class="alert alert-danger">
-                                <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif 
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
+            <!-- /.col-lg-12 -->
         </div>
+        <!-- /.row -->
+        <div class="row"> 
+            <div class="col-lg-12">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <form role="form" action="{{ url('admin/brands/'.$brand['id']) }}" method="post">
+                                    {{ csrf_field() }}
+                                    {{ method_field('patch') }}
+                                    <div class="form-group">
+                                        <label>Text Input</label>
+                                        <input class="form-control" name="brand_name" value="{{ $brand['brand_name'] }}">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Update</button>
+                                </form>
+                            </div>
+                            <!-- /.col-lg-12 (nested) -->
+                        </div>
+                        <!-- /.row (nested) -->
+                    </div>
+                    @if(count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif 
+                    <!-- /.panel-body -->
+                </div>
+                <!-- /.panel -->
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+    </div>
 @endsection
--- resources/views/backend/layouts/nav.blade.php
+++ resources/views/backend/layouts/nav.blade.php
@@ -109,3 +109,4 @@
             </div>
             <!-- /.navbar-static-side -->
         </nav>
+        
