e395ee409086264a6748321ca91bf84ec41da1d3
vagrant <vagrant@homestead>
2016-04-28 03:52:31 +0000

    Frontend home page

--- app/Http/Controllers/Frontend/ProductsController.php
+++ app/Http/Controllers/Frontend/ProductsController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Models\Frontend\Product;
+use DB;
 
 class ProductsController extends Controller
 {
@@ -28,15 +29,24 @@ class ProductsController extends Controller
 
         return view('frontend.dashboard.detailProduct', compact('product', 'productLatest'));
     }
+    /**
+    * Display list products
+    *
+    * @return array
+    */
     public function listAllProducts()
     {
         $productLatest = Product::with('brands')->take(5)
                                                 ->orderBy('created_at')
                                                 ->get();
 
-        // $listFeaturedProducts = Rating::with('products')->take(10)->orderBy('created_at')->get();
+        $listFeaturedProducts = DB::table('rating') ->join('products', 'rating.product_id', '=', 'products.id')
+                                                    ->select('products.*', DB::raw('SUM(rating.score) as rating'))
+                                                    ->groupBy('rating.product_id')
+                                                    ->orderBy('rating', 'desc')
+                                                    ->get();
         $listLatestProducts = Product::orderBy('id', 'DESC')->get();
         // $listBestsellerProducts = Order::with('products')->take(10)->orderBy('created_at')->get();
-        return view('frontend.dashboard.index', compact('listLatestProducts', 'productLatest'));
+        return view('frontend.dashboard.index', compact('listLatestProducts', 'productLatest', 'listFeaturedProducts'));
     }
 }
