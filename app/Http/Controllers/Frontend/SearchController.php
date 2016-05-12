287b272b0a454ce1439e0eec72bc0ee79fa64074
at-hoangpt <hoangpt@asiantech.vn>
2016-05-12 03:23:08 +0000

    Fix paginate in search function

--- app/Http/Controllers/Frontend/SearchController.php
+++ app/Http/Controllers/Frontend/SearchController.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use DB;
-use Input;
+use Illuminate\Support\Facades\Input;
 
 class SearchController extends Controller
 {
@@ -17,13 +17,18 @@ class SearchController extends Controller
     public function index()
     {
         $search = \Request::get('search');
+
         $products = DB::table('products');
+        
         $results = $products->join('brands', 'products.brand_id', '=', 'brands.id')
                             ->where('products.name', 'LIKE', '%'. $search .'%')
                             ->orWhere('products.description', 'LIKE', '%'. $search .'%')
                             ->orWhere('brands.brand_name', 'LIKE', '%'. $search .'%')
                             ->paginate(10);
-     
+
+        $results->appends(['search' => Input::get('search')])
+                ->render();
+
         return view('frontend.dashboard.search', compact('results'));
      
     }
