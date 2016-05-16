49a77e94d257829c057ca8c502966b77f5b6b9b5
at-phucnvh <phucnvh@asiantech.vn>
2016-05-16 02:51:55 +0000

    fix CircleCI

--- app/Http/Controllers/Frontend/OrdersController.php
+++ app/Http/Controllers/Frontend/OrdersController.php
@@ -32,19 +32,11 @@ class OrdersController extends Controller
      */
     public function search(Request $request)
     {
-        //$orderId = $request->searchorders;
-        //$email = $request->email;
-        //$user = User::where('email', $email)->first();
-        //$orderItem = Order::where('id', $orderId)->where('user_id', $user->id)->first();
-
-        $data = DB::table('orders')
-            ->join('users', 'orders.user_id', '=', 'users.id')
-            ->join('orders', 'orders.id', '=', 'orders.id')
-            ->select('orders.id as id', 'posts.name as post_name', 'users.name as user_name', 'image', 'categories.name as cate_name', 'posts.created_at as day_created', 'views')
-            ->paginate(5);
-
-        return view('backend.pages.post.list', compact('data'));
+        $orderId = $request->searchorders;
+        $email = $request->email;
+        $user = User::where('email', $email)->first();
+        $orderItem = Order::where('id', $orderId)->where('user_id', $user->id)->first();
         
-        //return view('frontend.orders.index', compact('orderItem'));
+        return view('frontend.orders.index', compact('orderItem'));
     }
 }
