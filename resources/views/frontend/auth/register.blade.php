0bd24a9c31dff6f5fe330b8a6ff225fee46f1566
at-hoangpt <hoangpt@asiantech.vn>
2016-05-18 04:59:31 +0000

    Web Login Unit Test

--- resources/views/frontend/auth/register.blade.php
+++ resources/views/frontend/auth/register.blade.php
@@ -44,36 +44,66 @@
                   <td>Name:</td>
                   <td>
                     <input type="text" class="large-field" name="name" value="{{ old('name') }}">
+                    @if ($errors->has('name'))
+                      <span class="help-block">
+                          <strong>{{ $errors->first('name') }}</strong>
+                      </span>
+                    @endif
                   </td>
                 </tr>
                 <tr>
                   <td>E-Mail:</td>
                   <td>
                     <input type="text" class="large-field" name="email" value="{{ old('email') }}">
+                    @if ($errors->has('email'))
+                      <span class="help-block">
+                          <strong>{{ $errors->first('email') }}</strong>
+                      </span>
+                    @endif
                   </td>
                 </tr>
                 <tr>
                   <td>Telephone:</td>
                   <td>
                     <input type="text" class="large-field" name="phone" value="{{ old('phone') }}">
+                    @if ($errors->has('phone'))
+                      <span class="help-block">
+                          <strong>{{ $errors->first('phone') }}</strong>
+                      </span>
+                    @endif
                   </td>
                 </tr>
                 <tr>
                   <td>Address:</td>
                   <td>
                     <input type="text" class="large-field" name="address" value="{{ old('address') }}">
+                    @if ($errors->has('address'))
+                      <span class="help-block">
+                          <strong>{{ $errors->first('address') }}</strong>
+                      </span>
+                    @endif
                   </td>
                 </tr>
                 <tr>
                   <td>Password:</td>
                   <td>
                     <input type="password" class="large-field" name="password">
+                    @if ($errors->has('password'))
+                      <span class="help-block">
+                          <strong>{{ $errors->first('password') }}</strong>
+                      </span>
+                    @endif
                   </td>
                 </tr>
                 <tr>
                   <td>Password Confirm:</td>
                   <td>
                     <input type="password" class="large-field" name="password_confirmation">
+                    @if ($errors->has('password'))
+                      <span class="help-block">
+                          <strong>{{ $errors->first('password') }}</strong>
+                      </span>
+                    @endif
                   </td>
                 </tr>
               </tbody>
