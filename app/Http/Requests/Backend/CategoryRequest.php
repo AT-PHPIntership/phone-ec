001763f0cc1213ece98587fb51591214e99a4b97
at-quannk <quannk@asiantech.vn>
2016-05-25 02:59:43 +0000

    CategoryManager

---
+++ app/Http/Requests/Backend/CategoryRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Requests\Backend;
+
+use App\Http\Requests\Request;
+
+class CategoryRequest extends Request
+{
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        if ($this->segment(3)) {
+            return [
+                'cate_name' => 'required|min:3|unique:product_category,cate_name,'.$this->segment(3),
+                // 'txtcatename' => 'required|min:3|unique:category,cate_name,'.$this->segment(3),
+                'cate_image' => 'mimes:jpeg,jpg,png|image',
+            ];
+        } else {
+            return [
+                'parent_id' => 'required',
+                'cate_name' => 'required|min:3|unique:product_category,cate_name',
+                'cate_image' => 'mimes:jpeg,jpg,png|image',
+                
+            ];
+        }
+        
+    }
+    
+    /**
+     * Get the validation rules that apply to the request
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'parent_id.required' => 'Please enter choose Category',
+            'cate_name.required' => 'Please enter Category Name',
+            'cate_name.unique' => 'The Category Name  has already been taken',
+            'cate_image.image' => 'This file isn\'t Image',
+        ];
+    }
+}
---
+++ app/Models/Backend/Category.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models\Backend;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+
+    protected $table = "product_category";
+    protected $fillable = ['id', 'cate_name', 'cate_description', 'cate_status', 'cate_image', 'parent_id'];
+
+    /**
+     * Function list all categories
+     *
+     * @param int    $cates  cates
+     * @param int    $parent parent
+     * @param string $str    str
+     * @param int    $select select
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function cateParent($cates, $parent = 0, $str = "--", $select = 0)
+    {
+        foreach ($cates as $cate) {
+            $cateId = $cate['id'];
+            $cateName = $cate['cate_name'];
+            if ($cate['parent_id'] == $parent) {
+                if ($select != 0 && $cateId === $select) {
+                    echo "<option value='$cateId' selected='selected'>$str $cateName</option>";
+                } else {
+                    echo "<option value='$cateId'>$str $cateName</option>";
+                }
+                $this->cateParent($cates, $cateId, $str . "--");
+            }
+        }
+    }
+}
