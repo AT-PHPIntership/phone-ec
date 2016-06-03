881d12af8a41ceefdec7ace564096ee07014389d
at-quannk <quannk@asiantech.vn>
2016-06-03 02:41:20 +0000

    fix CircleCI

--- app/Http/Controllers/Backend/OrdersController.php
+++ app/Http/Controllers/Backend/OrdersController.php
@@ -47,7 +47,7 @@ class OrdersController extends Controller
     {
         $orders = Order::with('users')->findOrFail($id);
         $details = OrderDetails::with('products')->where('order_id', $id)->get();
-        return view('backend.orders.edit', compact('orders','details'));
+        return view('backend.orders.edit', compact('orders', 'details'));
     }
 
     /**
@@ -63,17 +63,17 @@ class OrdersController extends Controller
         $order = Order::with('users')->findOrFail($id);
         $data = $request->all();
         if ($order->update($data)) {
-            if($request->status == 1) {
-                Mail::send('backend.orders.mails.confirm',$data, function ($message) use($request){
-                   $message->to($request->email)->subject("Orders are confirmed");
+            if ($request->status == 1) {
+                Mail::send('backend.orders.mails.confirm', $data, function ($message) use ($request) {
+                    $message->to($request->email)->subject("Orders are confirmed");
                 });
-            }elseif($request->status == 2) {
-                Mail::send('backend.orders.mails.moved',$data, function ($message) use($request){
-                   $message->to($request->email)->subject("Orders was moved");
+            } elseif ($request->status == 2) {
+                Mail::send('backend.orders.mails.moved', $data, function ($message) use ($request) {
+                    $message->to($request->email)->subject("Orders was moved");
                 });
-            }elseif($request->status == 3) {
-                Mail::send('backend.orders.mails.shipper',$data, function ($message) use($request){
-                   $message->to($request->email)->subject("Orders was shipped successfully");
+            } elseif ($request->status == 3) {
+                Mail::send('backend.orders.mails.shipper', $data, function ($message) use ($request) {
+                    $message->to($request->email)->subject("Orders was shipped successfully");
                 });
             }
             $request->session()->flash('message', 'Order was updated successfully!');
