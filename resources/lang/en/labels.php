c27c5e8b7aa515b61ee5781e5495e0793e2c96f4
at-quannk <quannk@asiantech.vn>
2016-06-08 09:25:25 +0000

    fix file label laguage

--- resources/lang/en/labels.php
+++ resources/lang/en/labels.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'account_login' => 'Account Login',
-    'new_customer' => 'New Customer',
     'register_account' => 'Register Account',
-    'returning_customer' => 'Returning Customer'
+    'register_account' => 'Register Account',
+    'returning_customer' => 'Returning Customer',
     'forgot_password' => "Forgotten Password",
     'signin_facebook' => 'Sign in using Facebook'
 ];
